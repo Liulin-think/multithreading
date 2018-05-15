ba217f0d08e8ad23b8bece6c5a77f9855f28b5d1
liulin_think <570389384@qq.com>
2018-05-15 14:16:46 +0800

    sleep方法getId方法

---
+++ src/main/java/com/liulin/study/multithreading/Thread_01_04.java
@@ -0,0 +1,16 @@
+package com.liulin.study.multithreading;
+
+/**
+ * sleep方法
+ * getId方法
+ * @author liulin_think
+ *
+ */
+public class Thread_01_04 {
+	public static void main(String[] args) throws InterruptedException {
+		// sleep()方法的作用是让当前"正在执行的线程"休眠(暂停执行)指定的毫秒数
+		Thread.sleep(1000);
+		// getId()方法的作用是取得线程的唯一标识
+		Thread.currentThread().getId();
+	}
+}
