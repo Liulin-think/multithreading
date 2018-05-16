b567487d29b9a9fefece533684d9f7be597cceb6
liulin_think <570389384@qq.com>
2018-05-16 11:07:36 +0800

    【多个对象多个锁】以及【 synchronized方法与对象锁】

--- src/main/java/com/liulin/study/multithreading/o2/Thread_02_01.java
+++ src/main/java/com/liulin/study/multithreading/o2/Thread_02_01.java
@@ -2,11 +2,13 @@ package com.liulin.study.multithreading.o2;
 
 /**
  * 第二章:对象及变量的并发访问
- * 
+ * 多个对象多个锁:
+ * 多个线程分别访问同一个类中的多个对象的同一个synchronized类型的方法时,不需要等待,也就是异步.
+ * 因为多个线程访问多个对象,JVM会创建多个锁.
  * synchronized方法与对象锁:
  * 1.A线程先持有object对象的Lock锁,B线程如果这时调用object对象中synchronized类型的方法则需要等待,也就是同步,
  * 即使B线程调用的synchronized方法与A线程调用的synchronized方法不是同一个方法.
- * 2.A线程先持有object多选的Lock锁,C线程如果这时调用object对象中的非synchronized类型的方法则不需要等待,也就是异步
+ * 2.A线程先持有object多选的Lock锁,C线程如果这时调用object对象中的非synchronized类型的方法则不需要等待,也就是异步.
  * @author liulin_think
  *
  */
@@ -15,9 +17,11 @@ public class Thread_02_01 {
 	public static void main(String[] args) {
 		Thread_02_01 thread_02_01 = new Thread_02_01();
 		ModelClass modelClass = thread_02_01.new ModelClass();
+		ModelClass modelClass2 = thread_02_01.new ModelClass();
 		thread_02_01.new ThreadA(modelClass,"ThreadA").start();
 		thread_02_01.new ThreadB(modelClass,"ThreadB").start();
 		thread_02_01.new ThreadC(modelClass,"ThreadC").start();
+		thread_02_01.new ThreadB(modelClass2,"ThreadB2").start();
 	}
 	
 	class ModelClass{
